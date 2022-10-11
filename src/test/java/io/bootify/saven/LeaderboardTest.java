2f04d7468bcef4a61e6aff4a7cb798f66fe2a258
Hei Yee Kai <endurrus@gmail.com>
2022-10-11 13:15:26 +0800

    refactor(leaderboardtest.java, userleaderboardtest.java): reformatted and removed comments

--- src/test/java/io/bootify/saven/LeaderboardTest.java
+++ src/test/java/io/bootify/saven/LeaderboardTest.java
@@ -6,8 +6,6 @@ import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import javax.print.attribute.standard.Media;
-
 import com.nimbusds.jose.shaded.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -51,15 +49,11 @@ public class LeaderboardTest {
 	@MockBean
 	private JwtDecoder jwtDecoder;
 
-	// updateLeaderboardWithToken got error
-
 	@Test
 	public void getLeaderboardWithToken() throws Exception {
 		Leaderboard leaderboardtest = new Leaderboard();
 		leaderboard.save(leaderboardtest);
 
-		// String responseAsString = response.getContentAsString();
-		// LeaderboardDTO leaderboardarr[] = objectMapper.readValue(responseAsString, LeaderboardDTO[].class);
 		URI uri = new URI(baseUrl + port + "/api/leaderboards/" + leaderboardtest.getId());
 		RequestBuilder request = MockMvcRequestBuilders.get(uri).with(SecurityMockMvcRequestPostProcessors.jwt());
 		MockHttpServletResponse response = mockMvc.perform(request).andReturn().getResponse();

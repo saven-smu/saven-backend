64f3e7922442aa299085735d281b1bfdd5a79924
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2022-10-17 01:52:58 +0800

    feat(auth0 with rbac): implemented role based authentication only to user api

--- src/main/java/io/bootify/saven/rest/UserResource.java
+++ src/main/java/io/bootify/saven/rest/UserResource.java
@@ -6,9 +6,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import java.util.UUID;
 import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,24 +27,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserResource {
 
+    @Value("${auth0.audience_mgt}")
+     private String audience;
+     
+     @Value("${auth0.client_id}")
+     private String clientId;
+     
+     @Value("${auth0.client_secret}")
+     private String clientSecret;
+
+     @Value("${auth0.management_domain}")
+     private String url;
+
+     @Value("${auth0.role_id}")
+     private String role;
+
     private final UserService userService;
 
     public UserResource(final UserService userService) {
         this.userService = userService;
     }
-
+    
     @GetMapping
+    @PreAuthorize("hasAuthority('read:user')")    
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable final UUID id) {
+        System.out.println("HEllo"+SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString());
         return ResponseEntity.ok(userService.get(id));
     }
 
     @GetMapping("verifyUser/{email}")
     public ResponseEntity<UserDTO> getUserByEmail(@PathVariable final String email) {
+        AuthManagement mgtToken = new AuthManagement();
+        String token = mgtToken.getMgtToken(url, audience, clientId, clientSecret);
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+        mgtToken.addDefaultRole(url, id, token, role);
         return ResponseEntity.ok(userService.getUserByEmail(email));
     }
 
@@ -51,6 +76,7 @@ public class UserResource {
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('update:user')")
     public ResponseEntity<Void> updateUser(@PathVariable final UUID id,
             @RequestBody @Valid final UserDTO userDTO) {
         userService.update(id, userDTO);

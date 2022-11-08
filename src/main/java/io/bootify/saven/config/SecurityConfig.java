0048a81fd6ff0bfe5a09a417e21be60c76a704ad
Hei Yee Kai <endurrus@gmail.com>
2022-11-08 15:45:41 +0800

    fix(securityconfig.java): add shit

--- src/main/java/io/bootify/saven/config/SecurityConfig.java
+++ src/main/java/io/bootify/saven/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
    private static final String[] WHITE_LIST_URLS = {
         "/api/leaderboards",
         "/api/userLeaderboards/{\\d+}",
-        "/api/leaderboards/getLeaderboards/{\\d+}/{\\d+}"
+        "/api/leaderboards/getLeaderboards/{\\d}/{\\d}"
    };
    
    @Value("${auth0.audience}")
